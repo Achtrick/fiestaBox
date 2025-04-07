97972da76453157656cc00d663a7b286b95cfba2
achtrick <achrefmtir7@gmail.com>
2025-04-07 11:41:22 +0100

    implimented guards and routing

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,13 +1,31 @@
 import { Route } from '@angular/router';
 import { AlbumsComponent } from './core/pages/albums/albums.component';
+import { EventsComponent } from './core/pages/events/events.component';
 import { GalleryComponent } from './core/pages/gallery/gallery.component';
 import { LoginComponent } from './core/pages/login/login.component';
 import { RegisterComponent } from './core/pages/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const appRoutes: Route[] = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'albums', component: AlbumsComponent },
-  { path: 'gallery', component: GalleryComponent },
+  {
+    path: '',
+    component: EventsComponent,
+    canActivate: [AuthGuard],
+    data: { allowIfAuthenticated: true },
+  },
+  { path: 'albums/:event_id', component: AlbumsComponent },
+  { path: 'gallery/:event_id/:album_id', component: GalleryComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuard],
+    data: { allowIfAuthenticated: false },
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AuthGuard],
+    data: { allowIfAuthenticated: false },
+  },
   { path: '**', redirectTo: '' },
 ];
---
+++ apps/client/src/app/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const isLoggedIn = this.authService.isAuthenticated();
+    const allowIfAuthenticated = route.data['allowIfAuthenticated'] ?? true;
+
+    if (isLoggedIn && !allowIfAuthenticated) {
+      this.router.navigate(['']);
+      return false;
+    }
+
+    if (!isLoggedIn && allowIfAuthenticated) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  }
+}
